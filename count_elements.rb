7ee003ed297872ba3cdd9e15bca9b506152614e3
Tien Tran <tientran2407.vn@gmail.com>
2018-10-09 10:54:03 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,18 +1,12 @@
 def count_elements(array)
-  # code goes here
-  # create a hash
-  array_counting_hash = {}
-  # loop through array, if hash key is not exits add it to hash with value 1. 
-  # if it exists, value +1
-  array.each { |element| 
-    if array_counting_hash[:element] == nil 
-      array_counting_hash[:element] = 1
-    else
-      array_counting_hash[:element] += 1
-    end
-  }
-
-array_counting_hash
+  def count_elements(array)
+  #hash.new(0) create the default any key's value = 0
+  # {} just create key's value = nil
+  count = Hash.new(0)
   
+  array.each {|item| count[item] += 1 }
+  count
+
+end
 end
  

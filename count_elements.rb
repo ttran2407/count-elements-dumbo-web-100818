c37b638a1071b2e0f7da2de0271bab7b0777b321
Tien Tran <tientran2407.vn@gmail.com>
2018-10-09 10:52:10 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -4,10 +4,12 @@ def count_elements(array)
   array_counting_hash = {}
   # loop through array, if hash key is not exits add it to hash with value 1. 
   # if it exists, value +1
-  
-  
   array.each { |element| 
+    if array_counting_hash[:element] == nil 
+      array_counting_hash[:element] = 1
+    else
       array_counting_hash[:element] += 1
+    end
   }
 
 array_counting_hash

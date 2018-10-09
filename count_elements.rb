e3e992226efd993317419fa7e5b62cc80e1d14a0
github username <github email address>
2018-10-09 09:12:40 -0400

    nothing

--- count_elements.rb
+++ count_elements.rb
@@ -3,7 +3,7 @@ def count_elements(array)
   #hash.new(0) create the default any key's value = 0
   # {} just create key's value = nil
   count = Hash.new(0)
-  
+  var = 0
   array.each {|item| count[item] += 1 }
   count
 

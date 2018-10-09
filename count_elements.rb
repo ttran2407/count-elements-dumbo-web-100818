2f9a0006b43b8712497797a20987ad7d5c33deb3
Tien Tran <tientran2407.vn@gmail.com>
2018-10-09 10:35:42 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
   # code goes here
+  # create a hash
+  array_counting_hash = {}
+  # loop through array, if hash key is not exits add it to hash with value 1. 
+  loop
+  end
+  # if it exists, value +1
+  
 end
  
